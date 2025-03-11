0e496a20a1389877f27d32060849bf9f1f2004e6
Ahmad Zaky <humami359@gmail.com>
2025-03-11 08:53:24 +0700

    feat: update new Dashboard

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -1,15 +1,49 @@
+import sys
+from pathlib import Path
 import streamlit as st
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
+import datetime as dt
+import babel
 from babel.numbers import format_currency
-from pathlib import Path
-        
-# Mengatur title pada tab browser
+
+print("Python Version:", sys.version)
+print("Streamlit Version:", st.__version__)
+print("Pandas Version:", pd.__version__)
+print("Matplotlib Version:", matplotlib.__version__)
+print("Seaborn Version:", sns.__version__)
+print("Babel Version:", babel.__version__)
+
 st.set_page_config(page_title="E-Commerce Dashboard")
 
-# Load dataset
+BASE_DIR = Path(__file__).resolve().parent
+data_path = BASE_DIR / "all_data.csv"
+logo_path = BASE_DIR.parent / "logo.jpg"
+
+def remove_outliers(df, columns):
+    """
+    Menghilangkan outlier berdasarkan metode IQR (Interquartile Range).
+    
+    Parameters:
+    df (pd.DataFrame): DataFrame yang akan diproses.
+    columns (list): List nama kolom numerik yang akan difilter dari outlier.
+
+    Returns:
+    pd.DataFrame: DataFrame tanpa outlier.
+    """
+    df_cleaned = df.copy()
+    for col in columns:
+        Q1 = df_cleaned[col].quantile(0.25)
+        Q3 = df_cleaned[col].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        df_cleaned = df_cleaned[(df_cleaned[col] >= lower_bound) & (df_cleaned[col] <= upper_bound)]
+    
+    return df_cleaned
+
 datetime_cols = [
     "order_purchase_timestamp",
     "order_approved_at",
@@ -19,31 +53,14 @@ datetime_cols = [
     "shipping_limit_date",
     ]
 
-# Dapatkan path absolut dari lokasi file dashboard.py
-BASE_DIR = Path(__file__).resolve().parent
-data_path = BASE_DIR / "all_data.csv"
-logo_path = BASE_DIR.parent / "logo.jpg"
-
-# Load dataset
 all_df = pd.read_csv(data_path)
-
-# Konversi column Date
 all_df[datetime_cols] = all_df[datetime_cols].apply(pd.to_datetime)
 
-# Sidebar filter Date
-rounded_css = """
-<style>
-    .rounded-img {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        border-radius: 50%;
-        width: 80%;
-    }
-</style>
-"""
-st.sidebar.markdown(rounded_css, unsafe_allow_html=True)
+numeric_columns = ["price", "freight_value", "payment_value", "order_item_id"]
+all_df = remove_outliers(all_df, numeric_columns)
+
 st.sidebar.image(str(logo_path), use_container_width=True, caption="Logo")
+
 start_date, end_date = st.sidebar.date_input(
     "Time Period",
     [all_df["order_approved_at"].min(), all_df["order_approved_at"].max()],
@@ -51,14 +68,15 @@ start_date, end_date = st.sidebar.date_input(
     max_value=all_df["order_approved_at"].max()
 )
 
-# Filter Date
-all_df_filtered = all_df[(all_df["order_approved_at"] >= pd.Timestamp(start_date)) &  (all_df["order_approved_at"] <= pd.Timestamp(end_date))]
+all_df_filtered = all_df[(all_df["order_approved_at"] >= pd.Timestamp(start_date)) & (all_df["order_approved_at"] <= pd.Timestamp(end_date))]
+all_df_filtered = all_df_filtered.copy()
+
 daily_orders = all_df_filtered.resample("D", on="order_approved_at").agg({"order_id": "nunique"}).reset_index()
 daily_revenue = all_df_filtered.resample("D", on="order_approved_at").agg({"payment_value": "sum"}).reset_index()
+
 total_orders = daily_orders["order_id"].sum()
 total_revenue = format_currency(daily_revenue["payment_value"].sum(), "IDR", locale="id_ID")
 
-# Menampilkan title pada halaman
 st.header("E-Commerce Dashboard")
 
 # Menampilkan metrik utama
@@ -66,82 +84,93 @@ col1, col2 = st.columns(2)
 col1.metric("Total Orders", total_orders)
 col2.metric("Total Revenue", total_revenue)
 
-# # Visualisasi pesanan harian
-# st.subheader("Daily Orders")
-# fig, ax = plt.subplots(figsize=(10, 5))
-# sns.lineplot(data=daily_orders, x="order_approved_at", y="order_id", marker="o", ax=ax)
-# ax.set_title("Orders Over Time")
-# ax.set_xlabel("Date")
-# ax.set_ylabel("Number of Orders")
-# st.pyplot(fig)
+# Sidebar menu untuk memilih tipe analisis
+analysis_type = st.sidebar.selectbox(
+    "Pilih Analisis",
+    [
+        "Daily Order & Revenue",
+        "Monthly Order Trends",
+        "Most Preferred Payment Method",
+        "Customer Segmentation Based on RFM",
+        "Top Product Categories by Sales Volume",
+        "Product pricing impact on customer ratings"
+    ]
+)
 
-# # Visualisasi pendapatan harian
-# st.subheader("Daily Revenue")
-# fig, ax = plt.subplots(figsize=(10, 5))
-# sns.lineplot(data=daily_revenue, x="order_approved_at", y="payment_value", marker="o", ax=ax)
-# ax.set_title("Revenue Over Time")
-# ax.set_xlabel("Date")
-# ax.set_ylabel("Revenue")
-# st.pyplot(fig)
+colors5 = ["#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4"]
+colors10 = [
+    "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4",
+    "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4"
+    ]
 
 
-# Section Bagaimana tren jumlah pesanan per bulan?
-with st.container():
+# Default: Visualisasi Pesanan Harian & Pendapatan Harian
+if analysis_type == "Daily Order & Revenue": 
+    # Visualisasi pesanan harian
+    st.subheader("Daily Orders")
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.lineplot(data=daily_orders, x="order_approved_at", y="order_id", marker="o", ax=ax)
+    ax.set_title("Orders Over Time")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Number of Orders")
+    st.pyplot(fig)
+
+    # Visualisasi pendapatan harian
+    st.subheader("Daily Revenue")
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.lineplot(data=daily_revenue, x="order_approved_at", y="payment_value", marker="o", ax=ax)
+    ax.set_title("Revenue Over Time")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Revenue")
+    st.pyplot(fig)
+
+# Analisis: Tren Jumlah Pesanan Per Bulan
+elif analysis_type == "Monthly Order Trends":
     st.subheader("Monthly Order Trends")
-    
-    all_df['month_year'] = all_df['order_purchase_timestamp'].dt.to_period('M')
-    monthly_df = all_df.groupby('month_year')['order_id'].count().reset_index()
+
+    all_df_filtered['month_year'] = all_df_filtered['order_purchase_timestamp'].dt.to_period('M')
+
+    monthly_df = all_df_filtered.groupby('month_year')['order_id'].count().reset_index()
     monthly_df.rename(columns={'order_id': 'order_count'}, inplace=True)
     monthly_df['month_year'] = monthly_df['month_year'].astype(str)
 
-    fig = px.line(monthly_df, x='month_year', y='order_count')
-    fig.update_xaxes(title='Month')
-    fig.update_yaxes(title='Number of Orders')
-    fig.update_layout(
-        plot_bgcolor='#ffffff',
-    )
-    st.plotly_chart(fig, use_container_width=True)
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.lineplot(data=monthly_df, x='month_year', y='order_count', marker='o', ax=ax)
 
-# Sections Metode pembayaran apa yang paling banyak digunakan?
-with st.container():
+    ax.set_xlabel("Month", fontsize=12)
+    ax.set_ylabel("Number of Orders", fontsize=12)
+    ax.set_title("Monthly Order Trends", fontsize=14)
+    plt.xticks(rotation=45)
+    plt.grid(True)
+
+    st.pyplot(fig)
+
+
+# Analisis: Metode Pembayaran Paling Populer
+elif analysis_type == "Most Preferred Payment Method":
     st.subheader("Most Preferred Payment Method")
 
-    payment_counts = all_df['payment_type'].value_counts()
+    payment_counts = all_df_filtered['payment_type'].value_counts().reset_index()
+    payment_counts.columns = ['payment_type', 'order_count']
 
-    fig = px.bar(x=payment_counts.index, y=payment_counts.values)
-    fig.update_layout(xaxis_title='Metode Pembayaran', yaxis_title='Jumlah Transaksi')
-    fig.update_layout(
-        plot_bgcolor='#ffffff',
-    )
-    st.plotly_chart(fig, use_container_width=True)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(x='payment_type', y='order_count', data=payment_counts, ax=ax)
 
-# Section Bagaimana rating rata-rata berdasarkan kategori produk?
-with st.container():
-    st.subheader("Customer Satisfaction Score")
+    ax.set_title("Most Preferred Payment Method", fontsize=14)
+    ax.set_xlabel("Payment Method", fontsize=12)
+    ax.set_ylabel("Order Count", fontsize=12)
+    plt.xticks(rotation=45)
 
-    average_rating_by_category = all_df.groupby('product_category_name')['review_score'].mean().reset_index()
-    average_rating_by_category = average_rating_by_category.rename(columns={'review_score': 'average_rating'})
-    top_10_categories = average_rating_by_category.sort_values(by='average_rating', ascending=False).head(10)
+    st.pyplot(fig)
 
-    fig = px.bar(top_10_categories, 
-        x='average_rating', 
-        y='product_category_name', 
-        orientation='h',
-        title='Top 10 Product Categories by Average Rating',
-        labels={'average_rating': 'Average Rating', 'product_category_name': 'Product Category'})
-    fig.update_layout(
-        plot_bgcolor='#ffffff',
-    )
-    st.plotly_chart(fig, use_container_width=True)
+# Analisis: Segmentasi Pelanggan Berdasarkan RFM
+elif analysis_type == "Customer Segmentation Based on RFM":
+    st.subheader("Customer Segmentation Based on RFM")
 
-# Section "Bagaimana segmentasi pelanggan berdasarkan RFM Analysis?"
-with st.container():
-    st.subheader("Customer Segmentation Based on")
+    all_df_filtered['total_price'] = all_df_filtered['price'] * all_df_filtered['order_item_id']
+    all_df_filtered.rename(columns={'order_purchase_timestamp': 'order_date'}, inplace=True)
 
-    all_df['total_price'] = all_df['price'] * all_df['order_item_id']
-    all_df.rename(columns={'order_purchase_timestamp': 'order_date'}, inplace=True)
-
-    rfm_df = all_df.groupby(by="customer_id", as_index=False).agg({
+    rfm_df = all_df_filtered.groupby(by="customer_id", as_index=False).agg({
         "order_date": "max",
         "order_id": "nunique",
         "total_price": "sum"
@@ -149,7 +178,7 @@ with st.container():
     rfm_df.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
 
     rfm_df["max_order_timestamp"] = rfm_df["max_order_timestamp"].dt.date
-    recent_date = all_df["order_date"].dt.date.max()
+    recent_date = all_df_filtered["order_date"].dt.date.max()
     rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
 
     rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
@@ -158,88 +187,60 @@ with st.container():
 
     with tab1:
         fig, ax = plt.subplots(figsize=(10, 6))
-
-        # Bar plot for Recency
-        colors = ["#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4"]
-        sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="recency", ascending=False).head(5), hue="customer_id", palette=colors, dodge=False, ax=ax)
-        ax.set_ylabel(None)
-        ax.set_xlabel(None)
-        ax.set_title("By Recency (days)", loc="center", fontsize=18)
-        ax.tick_params(axis='x', labelsize=15)
-        ax.legend([],[], frameon=False)
+        sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="recency", ascending=False).head(5), hue="customer_id", palette=colors5, ax=ax)
+        ax.set_ylabel("Recency")
+        ax.set_xlabel("Customer ID")
+        ax.set_title("By Recency (days)", fontsize=14)
         st.pyplot(fig)
-        
 
     with tab2:
         fig, ax = plt.subplots(figsize=(10, 6))
-
-        # Bar plot for Frequency
-        colors = ["#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4"]
-        sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), hue="customer_id", palette=colors, dodge=False, ax=ax)
-        ax.set_ylabel(None)
-        ax.set_xlabel(None)
-        ax.set_title("By Frequency", loc="center", fontsize=18)
-        ax.tick_params(axis='x', labelsize=15)
-        ax.legend([],[], frameon=False)
+        sns.barplot(y="frequency", x="customer_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), hue="customer_id", palette=colors5, ax=ax)
+        ax.set_ylabel("Frequency")
+        ax.set_xlabel("Customer ID")
+        ax.set_title("By Frequency", fontsize=14)
         st.pyplot(fig)
-        
+
     with tab3:
         fig, ax = plt.subplots(figsize=(10, 6))
-
-        # Bar plot for Monetary
-        colors = ["#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4"]
-        sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), hue="customer_id", palette=colors, dodge=False, ax=ax)
-        ax.set_ylabel(None)
-        ax.set_xlabel(None)
-        ax.set_title("By Monetary", loc="center", fontsize=18)
-        ax.tick_params(axis='x', labelsize=15)
-        ax.legend([],[], frameon=False)
+        sns.barplot(y="monetary", x="customer_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), hue="customer_id", palette=colors5, ax=ax)
+        ax.set_ylabel("Monetary")
+        ax.set_xlabel("Customer ID")
+        ax.set_title("By Monetary", fontsize=14)
         st.pyplot(fig)
 
-# Section Bagaimana distribusi geografis pesanan berdasarkan lokasi pelanggan?
-with st.container():
-    st.subheader("Geographical Distribution of Orders")
-
-    order_distribution = all_df.groupby(['customer_city', 'customer_state'])['order_id'].count().reset_index()
-    order_distribution.rename(columns={'order_id': 'order_count'}, inplace=True)
-    st.dataframe(order_distribution)
-    fig = px.choropleth(order_distribution,
-                        locations='customer_state',
-                        locationmode="country names",
-                        color='order_count',
-                        hover_name='customer_city',
-                        title='Distribusi Geografis Pesanan',
-                        color_continuous_scale='Viridis')
-
-    fig.update_geos(
-        fitbounds="locations",
-        visible=False,
-        scope="world",
-    )
-    fig.update_layout(
-            plot_bgcolor='#cacaca',
-        )
-    st.plotly_chart(fig, use_container_width=True)
-    
-with st.container():
+# Analisis: Kategori Produk dengan Penjualan Terbanyak
+elif analysis_type == "Top Product Categories by Sales Volume":
     st.subheader("Top Product Categories by Sales Volume")
     
-    sales_by_category = all_df.groupby('product_category_name')['price'].sum().reset_index()
+    sales_by_category = all_df_filtered.groupby('product_category_name')['price'].sum().reset_index()
     sales_by_category.rename(columns={'price': 'total_sales'}, inplace=True)
     sales_by_category = sales_by_category.sort_values(by='total_sales', ascending=False)
 
-    # Select the top N categories (e.g., top 10)
     top_n = 10
     top_categories = sales_by_category.head(top_n)
     st.dataframe(top_categories)
     
-    fig = plt.figure(figsize=(12, 6))
-    sns.barplot(y=top_categories['product_category_name'], x=top_categories['total_sales'],
-                dodge=False, legend=False)
-    plt.xlabel("Total Sales")
-    plt.ylabel("Category Name")
-    plt.title(f"Top {top_n} Product Categories by Sales Volume")
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.barplot(y=top_categories['product_category_name'], x=top_categories['total_sales'], hue=top_categories["product_category_name"], palette=colors10, ax=ax, legend=False)
+    ax.set_xlabel("Total Sales")
+    ax.set_ylabel("Category Name")
+    ax.set_title(f"Top {top_n} Product Categories by Sales Volume")
 
-    st.pyplot(fig )
-    
+    st.pyplot(fig)
+
+elif analysis_type == "Product pricing impact on customer ratings":
+    st.subheader("Product Pricing Impact on Customer Ratings")
+
+    price_rating_df = all_df_filtered[["product_id", "price", "review_score"]].dropna()
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(x="review_score", y="price", data=price_rating_df, ax=ax, hue="review_score", palette=colors5, legend=False)
+
+    ax.set_title("Product Pricing Impact on Customer Ratings", fontsize=14)
+    ax.set_xlabel("Customer Rating", fontsize=12)
+    ax.set_ylabel("Product Price (IDR)", fontsize=12)
+
+    st.pyplot(fig)
+
 st.markdown("> Created by Ahmad Zaky Humami MC009D5Y0493")
